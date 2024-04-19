d11c62cfbacb8c17b0eae537d9a8da3e0ddee6cf
Thel-nny <thelanny.maguillano-22@cpu.edu.ph>
2024-04-19 22:21:26 +0800

    frontend for login

--- pogs-app/frontend/src/pages/login.tsx
+++ pogs-app/frontend/src/pages/login.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios'
-import useSignIn from 'react-auth-kit/hooks/useSignIn';
 import {useNavigate} from 'react-router-dom'
 
 // Define the type for the form data
@@ -54,19 +53,26 @@ const LoginForm: React.FC = () => {
 
 
  return (
-    <form onSubmit={handleSubmit}>
-      <label>
+  <section className=' w-full h-screen bg-gray-dark dark:bg-gray-900'>
+    <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+        <div className='p-6 space-y-2 md:space-y-6 sm:p-8'>
+          <h1 className='text-2xl font-bold leading-tight tracking-tight md:text-2xl'> Log In</h1>
+      <form className='space-y-4 md:space-y-6'onSubmit={handleSubmit}>
+        <label className='block mb-2 text-md font-medium text-gray-900'>
         Email:
-        <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
+        <input className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-4' type="email" name="email" value={formData.email} onChange={handleInputChange} />
+        </label>
+        <label className='block mb-2 text-md font-medium text-gray-900'>
         Password:
-        <input type="password" name="password" value={formData.password} onChange={handleInputChange} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
+        <input className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-4' type= "password" name="password" value={formData.password} onChange={handleInputChange} />
+        </label>
+        <button className='border rounded-lg p-2 w-full text-white bg-primary-400 hover:bg-primary-900' type="submit">Login</button>
+      </form>
+      </div>
+      </div>
+     </div>
+    </section>
  );
 };
 
